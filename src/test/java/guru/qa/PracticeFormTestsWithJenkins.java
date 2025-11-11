0d9454e0f48519f0afac8e8d995215bf75e926c7
Aleksandr Rozhkov <dir.gefest@gmail.com>
2025-11-11 23:08:13 +0300

    screenshot attach

--- src/test/java/guru/qa/PracticeFormTestsWithJenkins.java
+++ src/test/java/guru/qa/PracticeFormTestsWithJenkins.java
@@ -2,7 +2,9 @@ package guru.qa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -16,73 +18,78 @@ import static io.qameta.allure.Allure.step;
 public class PracticeFormTestsWithJenkins {
 
     @BeforeAll
-    static void beforeAll(){
+    static void setUp() {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
+    AfterEach
+    void afterEach(){
+        Attach.screenshotAs("Last screenshot");
+    }
+
 
     @Tag("homeWork")
     @Test
-    void positiveFillPracticeFormTest(){
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        step("Открываем страницу формы", () ->{
+    void positiveFillPracticeFormTest() {
+        step("Открываем страницу формы", () -> {
             open("automation-practice-form");
             executeJavaScript("$('#fixedban').remove()");
             executeJavaScript("$('footer').remove()");
-        } );
-        step("Задаем имя: ", () ->{
+        });
+        step("Задаем имя: ", () -> {
             $("#firstName").setValue("Ivan");
         });
         step("Задаем фамилию: ", () -> {
             $("#lastName").setValue("Ivanov");
         });
-        step("Задаем электронную почту: ", () ->{
+        step("Задаем электронную почту: ", () -> {
             $("#userEmail").setValue("Something@mail.org");
         });
-            step("Задаем пол: ", () ->{
-                $("#genterWrapper").$(byText(("Male"))).click();
-            });
-        step("Задаем номер телефона", () ->{
+        step("Задаем пол: ", () -> {
+            $("#genterWrapper").$(byText(("Male"))).click();
+        });
+        step("Задаем номер телефона: ", () -> {
             $("#userNumber").setValue("9169284215");
         });
-        step("Задаем дату рождения: ", () ->{
+        step("Задаем дату рождения: ", () -> {
             $("#dateOfBirthInput").click();
             $(".react-datepicker__month-select").selectOption("November");
             $(".react-datepicker__year-select").selectOption("2000");
             $(".react-datepicker__day--020").click();
         });
-        step("Задаем предмет: ", () ->{
+        step("Задаем предмет: ", () -> {
             $("#subjectsInput").setValue("English").pressEnter();
         });
-        step("Задаем хобби: ", () ->{
+        step("Задаем хобби: ", () -> {
             $("#hobbiesWrapper").$(byText("Reading")).click();
         });
-        step("Задаем картинку: ", () ->{
+        step("Задаем картинку: ", () -> {
             $("#uploadPicture").uploadFromClasspath("Picture1.png");
         });
-        step("Задаем адрес: ", () ->{
+        step("Задаем адрес: ", () -> {
             $("#currentAddress").setValue("Something address");
         });
-        step("Задаем штат: ", () ->{
+        step("Задаем штат: ", () -> {
             $("#state").click();
             $("#stateCity-wrapper").$(byText("Rajasthan")).click();
         });
-        step("Задаем город: ", () ->{
+        step("Задаем город: ", () -> {
             $("#city").click();
             $("#stateCity-wrapper").$(byText("Jaiselmer")).click();
         });
-        step("Отправляем форму", () ->{
+        step("Отправляем форму", () -> {
             $("#submit").click();
         });
 
 
-
-        step("Ожидаем, что модальное окно с введенными данными открылось", () ->{
+        step("Ожидаем, что модальное окно с введенными данными открылось", () -> {
             $(".modal-dialog").should(appear);
         });
-        step("Ожидаем совпадение с заданными данными", () ->{
+        step("Ожидаем совпадение с заданными данными", () -> {
             $(".modal-body").shouldHave(text("Ivan"));
             $(".modal-body").shouldHave(text("Ivanov"));
             $(".modal-body").shouldHave(text("Something@mail.org"));
