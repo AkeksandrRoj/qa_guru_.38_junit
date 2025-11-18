55572139df482ccc2de21d5273ec60fdcf963553
Admin <dir.gefest@gmail.com>
2025-11-18 20:39:24 +0300

    add SystemPropertiesTests

---
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -0,0 +1,27 @@
+package properties.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    void systemPropertiesTest(){
+        System.setProperty("browser", "chrome");
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties2Test(){
+        System.setProperty("browser", "chrome");
+        String browser = System.getProperty("browser", "mozilla");
+        System.out.println(browser);
+    }
+    @Test
+    @Tag("property")
+    void systemProperties3Test(){
+        String browser = System.getProperty("browser", "mozilla");
+        System.out.println(browser);
+    }
+
+}
